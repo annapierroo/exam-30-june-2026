2889c1d414bd125fbe278db56ccb14e2de85fd2b
fabio <fabio.tiberio@ht-x.com>
2026-06-21 14:36:26 +0200

    Export advanced heuristic policy

--- policy/__init__.py
+++ policy/__init__.py
@@ -1,8 +1,15 @@
 """Policy interfaces and implementations."""
 
+from .advanced_heuristic_policy import AdvancedHeuristicPolicy
 from .base import Policy
 from .greedy_policy import GreedyPolicy
 from .heuristic_policy import HeuristicPolicy
 from .random_policy import RandomPolicy
 
-__all__ = ["GreedyPolicy", "HeuristicPolicy", "Policy", "RandomPolicy"]
+__all__ = [
+    "AdvancedHeuristicPolicy",
+    "GreedyPolicy",
+    "HeuristicPolicy",
+    "Policy",
+    "RandomPolicy",
+]
